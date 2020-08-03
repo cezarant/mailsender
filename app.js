742d5dc316d6b9bf61fdf358640caab7941a6308
Cezar <cezar.sousa@sysmap.com.br>
2020-08-03 17:34:00 -0300

    publicacao com parametros

--- app.js
+++ app.js
@@ -3,19 +3,23 @@ const nodemailer = require("nodemailer");
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+const smtpGoogle = process.env.SMTPGOOGLE;
+const postSMTPGoogle =  process.env.PORTASMTPGOOGLE;
+const assuntoEmail   = "Email de contato";
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const port = process.env.PORT || 3000;
+
 const server = app.listen(port, function ()
 {
     console.log('Servidor escutando em ' + port);
 });
 /* Trecho que envia email */
-async function main(vs_rementente,corpoEmail){ 
+async function envioEmail(vs_rementente,corpoEmail){ 
     let transporter = nodemailer.createTransport({
-        host: 'smtp.googlemail.com', 
-        port: 465, 
+        host: smtpGoogle , 
+        port: postSMTPGoogle, 
         secure: true, 
         auth: {
             user: process.env.USUARIOGMAIL, 
@@ -24,20 +28,21 @@ async function main(vs_rementente,corpoEmail){
     }); 
     
     let info = await transporter.sendMail({
-        from: process.env.USUARIOGMAIL, 
-        to: vs_rementente,
-        subject: "Email de contato", // Subject line        
-        html: corpoEmail, // html body
-    }); 
-    console.log("Message sent: %s", info.messageId);	
+        from:    process.env.USUARIOGMAIL, 
+        to:      vs_rementente,
+        subject: assuntoEmail, 
+        html:    corpoEmail,
+    });     
 }
-
 app.get('/', function(req, res)
 {    
 	res.send("Enviador de email pronto para ser utilizado...");	
 });
-
+/** Rota para enviar os emails **/
 app.get('/email', function(req, res)
-{    
-	main("cezarantsouza@gmail.com",'Corpo de email Teste').then(res.send("So alegria")).catch(console.error);	
+{    	 
+	envioEmail(req.query.destinario,
+			   req.query.campoEmail)
+			  .then(res.send("Sucesso!"))
+		      .catch(console.error);	
 });
